af8015e82d283350b8a831a2a287e2bc33d30cd9
Sundeep Mallya <sxm1972@users.noreply.github.com>
2017-04-08 16:22:39 +0530

    Create mkjson.bat
    Create mkjson.bat

---
+++ BuildScripts/mkjson.bat
@@ -0,0 +1,1 @@
+FOR /F "eol=; tokens=1,2,3,4,5,6* delims=," %%i in (%CMTEMPLATES%\configdata.txt) do SET XXX7=%%j & sed --text -e "s/XXX1/%%i/" -e "s/XXX2/%%j/" -e "s/XXX3/%%k/" -e "s/XXX4/%%l/" -e "s/XXX5/%%m/" -e "s/XXX6/%%n/"  -e "s/XXX7/%%jXXX7/;s/sXXX7//;s/XXX7//" %CMTEMPLATES%\AddModifyDeleteTemplate.json > cm-%%i-%%l.json
